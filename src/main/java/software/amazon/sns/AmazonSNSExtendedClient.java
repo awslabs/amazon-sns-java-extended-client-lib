1647ee7a854ac7c80feba32d0cf5bef65855ceee
Tianlu Shi <tianls@amazon.com>
2023-06-15 14:20:49 -0700

    Revert "Close s3client to avoid memory leak"
    This reverts commit 1cf9332d7d601368f2006ae96e298b988ded9a69.

--- src/main/java/software/amazon/sns/AmazonSNSExtendedClient.java
+++ src/main/java/software/amazon/sns/AmazonSNSExtendedClient.java
@@ -275,10 +275,4 @@ public class AmazonSNSExtendedClient extends AmazonSNSExtendedClientBase {
         publishRequestBuilder.messageAttributes(attributes);
         return publishRequestBuilder.build();
     }
-
-    @Override
-    public void close() {
-        super.close();
-        this.snsExtendedClientConfiguration.getS3Client().close();
-    }
 }
